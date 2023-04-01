4a2a76751161c4207a93cbd317df0fc47549eb95
Will V <wmvernon@proton.me>
2023-04-01 18:25:30 -0400

    Add - Files

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
+
+const config: DocsThemeConfig = {
+	logo: <span>My Project</span>,
+	project: {
+		link: 'https://github.com/willvernon/TypeForm',
+	},
+	chat: {
+		link: 'https://discord.com',
+	},
+	docsRepositoryBase: 'https://github.com/willvernon/TypeForm',
+	footer: {
+		text: 'Nextra Docs Template',
+	},
+};
+
+export default config;
